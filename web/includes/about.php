b2715cc22e04dfa26491e4abe00d7ad24ff5637e
muzamil <muzzimsyed21users.noreply.github.com>
2015-07-30 11:13:12 -0700

    about me

--- web/includes/about.php
+++ web/includes/about.php
@@ -33,8 +33,13 @@
   			<div class="info" class=".col-xs-12 .col-sm-6 .col-md-8">
 
   					&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp 
-  					My name is Muzamil Syed. I am a senior at the University of California, Irvine. I am currently pursuing a Bachelor
-  					of Computer Science degree with a minor in Informatics. 
+  					My name is Muzamil Syed. I was born in Chicago, IL where I spent the first 3 years of my life before moving to Texas and spending the next 10 years of my childhood. 
+  					It was within these years where I developed an ambitious interest in computers. I received my first computer around fourth grade and even though this computer does not 
+  					match the capacity of computers today I was amazed at what tasks it can accomplish.  
+  					I am a senior at the University of California, Irvine. I am currently pursuing a Bachelor
+  					of Computer Science degree with a minor in Informatics. Ever since I have received my first computer as a child living 
+  					in Texas 
+  					I am 
   			</div>				
 		
 		</div>
