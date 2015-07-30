6fe8cc7fe7dd43ffa1d62a4d7fac0af9744550a0
muzamil <muzzimsyed21users.noreply.github.com>
2015-07-30 12:28:07 -0700

    about me

--- web/includes/about.php
+++ web/includes/about.php
@@ -33,13 +33,23 @@
   			<div class="info" class=".col-xs-12 .col-sm-6 .col-md-8">
 
   					&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp 
-  					My name is Muzamil Syed. I was born in Chicago, IL where I spent the first 3 years of my life before moving to Texas and spending the next 10 years of my childhood. 
-  					It was within these years where I developed an ambitious interest in computers. I received my first computer around fourth grade and even though this computer does not 
-  					match the capacity of computers today I was amazed at what tasks it can accomplish.  
-  					I am a senior at the University of California, Irvine. I am currently pursuing a Bachelor
-  					of Computer Science degree with a minor in Informatics. Ever since I have received my first computer as a child living 
-  					in Texas 
-  					I am 
+
+  					I am currently a student at the University of California, Irvine where I'm in the process of pursuing a Bachelors Degree in Computer Science with a minor in Informatics. 
+  					My specialization emphasizes Information Systems where I learned from courses such as database management and information retrieval. Additionally, I have taken courses in 
+  					front end user interface design. These combination of classes have allowed my to take interest in Web Development at both the front and back end. I am also interested in 
+  					the development of software and computer information systems. 
+
+  					<br><br>
+
+
+
+  <!-- 					My name is Muzamil Syed. I was born in Chicago, IL where I spent the first 3 years of my life before moving to Texas and spending the next 10 years of my childhood. 
+  					It was within these years where I developed an ambitious interest in computers. I received my first computer in fourth grade. Even though this computer did not 
+  					match the capacity of computers today, I was amazed at what tasks it can accomplish and knew that someday I would be a part of its advancements. 
+  					 Up until now I have been satisfying my curiousity about technology by downloading and comparing software functionalities, creating webpages, and learning the hardware components of a computer's interior.
+ -->
+
+ 					In my free time I enjoy playing sports such as football/basketball, working out, hiking, going to the beach, and eating.
   			</div>				
 		
 		</div>
