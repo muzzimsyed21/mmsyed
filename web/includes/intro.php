401dbf03699c5431b29d512f241d943d92ef89ed
muzamil <muzzimsyed21users.noreply.github.com>
2015-08-02 10:37:48 -0700

    intro slideshow test

--- web/includes/intro.php
+++ web/includes/intro.php
@@ -6,3 +6,19 @@
 		</div>	
     </section>
 	<!-- /Section: intro -->
+
+<script>
+	//Array of images which you want to show: Use path you want.
+var images=new Array('../img/chicago.jpg','../img/chicago.jpg');
+var nextimage=0;
+doSlideshow();
+
+function doSlideshow(){
+    if(nextimage>=images.length){nextimage=0;}
+    $('.intro')
+    .css('background-image','url("'+images[nextimage++]+'")')
+    .fadeIn(500,function(){
+        setTimeout(doSlideshow,1000);
+    });
+}
+</script>
